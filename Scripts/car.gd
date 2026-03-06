c3d98d91cd58a025ae25ef5c96594547f7835bfd
ZibiNandi01 <zibrinyinandi@gmail.com>
2026-03-06 13:00:30 +0100

    Halad, kanyarodik, a fek mukodik, egyszeru palya

--- Scripts/car.gd
+++ Scripts/car.gd
@@ -1,7 +1,9 @@
 extends VehicleBody3D
 
+
 @export var MAX_STEER = 0.6
-@export var ENGINE_POWER = 300
+@export var ENGINE_POWER = 5000
+@export var BRAKE_POWER = 25
 
 func _physics_process(delta):
 	if steering>0:
@@ -17,4 +19,6 @@ func _physics_process(delta):
 	else:
 		steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta *2)
 			
-	engine_force = Input.get_axis("down", "up") * ENGINE_POWER
+	
+	engine_force = int(Input.is_action_pressed("up")) * ENGINE_POWER
+	brake = int(Input.is_action_pressed("down")) * BRAKE_POWER
